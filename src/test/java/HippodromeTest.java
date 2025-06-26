234b8c74279bb033f0a3bb88c51dd3b847636e38
Bekk I.V. <888_1@mail.ru>
2025-06-26 22:36:10 +0500

    Add 1 testMethods

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -2,16 +2,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class HippodromeTest {
     private static List<Horse> list;
@@ -34,18 +28,18 @@ class HippodromeTest {
     public void constructorWithNullArgumentHippodromeMessageTest() {
         try {
             new Hippodrome(null);
-        } catch (Exception e){
+        } catch (Exception e) {
             Assertions.assertEquals("Horses cannot be null.", e.getMessage());
         }
     }
 
     @Test
-    public void constructorWithNullListHippodromeTest(){
+    public void constructorWithNullListHippodromeTest() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> new Hippodrome(list));
     }
 
     @Test
-    public void constructorWithNullListHippodromeMessageTest(){
+    public void constructorWithNullListHippodromeMessageTest() {
         try {
             new Hippodrome(list);
         } catch (Exception e) {
@@ -56,7 +50,7 @@ class HippodromeTest {
     @Test
     void getHorses() {
         for (int i = 0; i < 30; i++) {
-            list.add(new Horse("Horse № "+ i, ThreadLocalRandom.current().nextDouble(0.2,3))); //проверить число
+            list.add(new Horse("Horse № " + i, ThreadLocalRandom.current().nextDouble(0.2, 3))); //проверить число
         }
         Assertions.assertEquals(30, list.size());
     }
@@ -78,7 +72,7 @@ class HippodromeTest {
     void getWinner() {
         int max = 4;
         for (int i = 0; i <= max; i++) {
-            list.add(new Horse("Horse № "+ i, 1, i));
+            list.add(new Horse("Horse № " + i, 1, i));
         }
         Assertions.assertEquals(new Hippodrome(list).getWinner(), list.get(max));
     }
